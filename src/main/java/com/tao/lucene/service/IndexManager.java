2f901edc004c9466e396b494a3eb6c0a2caccc17
taosm <samtao821030@gmail.com>
2017-01-16 17:59:03 +0800

    append

--- src/main/java/com/tao/lucene/service/IndexManager.java
+++ src/main/java/com/tao/lucene/service/IndexManager.java
@@ -1,8 +1,32 @@
 package com.tao.lucene.service;
 
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.HttpSolrServer;
+import org.apache.solr.common.SolrInputDocument;
+import org.junit.Test;
+
+import java.io.IOException;
+
 /**
  * Created by Administrator on 2017/1/12 0012.
  */
 public class IndexManager {
+    @Test
+    public void insertAndUpdateIndex() throws IOException, SolrServerException {
+        HttpSolrServer server = new HttpSolrServer("http://localhost:8080/solr");
+        SolrInputDocument doc = new SolrInputDocument();
+        doc.addField("id","c001");
+        doc.addField("name","solr test");
+        server.add(doc);
+        server.commit();
+    }
+
+    @Test
+    public void deleteIndex() throws IOException, SolrServerException{
+        HttpSolrServer server = new HttpSolrServer("http://localhost:8080/solr");
+
+        server.deleteById("c001");
+        server.commit();
+    }
 
 }
